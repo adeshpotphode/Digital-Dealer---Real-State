9c98876f0a6ee80ba7bbc013aa8ae6dbaf67be46
adeshpotphode <adeshpotphoe@gmail.com>
2025-04-10 00:54:05 +0530

    Updated PropController, added PropService, Repository, and visiterlist.html

--- src/main/java/com/example/Img_Store/PropController.java
+++ src/main/java/com/example/Img_Store/PropController.java
@@ -1,16 +1,24 @@
 package com.example.Img_Store;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PropController {
     
+    @Autowired
+    PropService ps;
+
     @GetMapping("/")
     public String index()
     {
         return "index";
     }
+
     @GetMapping("/AboutUs")
     public String aboutUs()
     {
@@ -26,10 +34,27 @@ public class PropController {
     {
         return "projects";
     }
+
     @GetMapping("/Contact")
-    public String contact()
+    public String contact(Model model)
     {
+        Prop prop = new Prop();
+        model.addAttribute("prop", prop);
         return "contact";
     }
+
+    @PostMapping("/SaveVisiter")
+    public String saveVisiter(@ModelAttribute("prop") Prop prop)
+    {
+        ps.save(prop);
+        return"redirect:/Contact";
+    }
+    
+     @GetMapping("/VisiterListAdmin")
+     public String data(Model model)
+     {
+         model.addAttribute("allInquiriesList", ps.getAllInquiries());
+         return "visiterlist";
+     }
 }
 
